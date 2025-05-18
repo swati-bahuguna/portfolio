3db7b4c930976d9d524c5487adfe7d1a5d043894
Mohit Uniyal <mohituniyal46@gmail.com>
2025-05-18 17:46:44 +0530

    first commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,92 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Python</li>
+        <li>SQL</li>
+        <li>MS Excel</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Graphic Era Hill University, Dehradun</li>
+        <li>PG College, Rishikesh</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Achievements",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>National Player Taekwondo Player</li>
+        <li>NCC Cadet - Leadership & Discipline Training</li>
+        <li>Crafting & Painting Entusiast</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            Aspiring business professional from Rishikesh with a B.Sc. in Math, Physics, and Geology, pursuing an MBA in Business Analytics & Marketing. A national-level Taekwondo player and NCC cadet, blending analytical skills, creativity, and leadership. Passionate about data-driven strategies to solve real-world business challenges and drive growth.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
